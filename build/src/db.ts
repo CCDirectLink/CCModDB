f6a0de4b1c193ee3db950e9659decb224c09f40a
krypek <krypek@tuta.io>
2024-02-07 17:54:56 +0100

    Add ccmod.json support

--- build/src/db.ts
+++ build/src/db.ts
@@ -1,225 +1,268 @@
-import semver from 'semver';
-import crypto from 'crypto';
-import fs from 'fs';
-import { download, streamToBuffer } from './download';
+import semver from 'semver'
+import crypto from 'crypto'
+import fs from 'fs'
+import { download, streamToBuffer } from './download'
+import { ModMetadatasInput, ModMetadatas } from './source'
 
 interface ModDb {
-	[name: string]: {
-		name: string,
-		description: string,
-		license?: string,
-		page: Page[],
-		archive_link: string,
-		hash: {
-			sha256: string,
-		},
-		version: string,
-	}
+    [name: string]: {
+        name: string
+        description: string
+        license?: string
+        page: Page[]
+        archive_link: string
+        hash: {
+            sha256: string
+        }
+        version: string
+    }
 }
 
-export async function build(packages: [PkgMetadata, InputLocation][]): Promise<PackageDB> {
-	const result: PackageDB = {};
-	const promises: Promise<void>[] = [];
+export async function build(packages: ModMetadatasInput[]): Promise<PackageDB> {
+    const result: PackageDB = {}
+    const promises: Promise<void>[] = []
 
-	for (const [, [pkg, inputs]] of groupByName(packages)) {
-		if (!check(pkg)) {
-			continue;
-		}
+    for (const [, { meta, ccmod, inputs }] of groupByName(packages)) {
+        if (ccmod && !checkCCMod(ccmod)) continue
+        if (meta && !checkMeta(meta)) continue
 
-		promises.push(buildEntry(result, pkg, inputs));
-	}
+        promises.push(buildEntry(result, meta, ccmod, inputs))
+    }
 
-	await Promise.all(promises);
+    await Promise.all(promises)
 
-	return sort(result);
+    return sort(result)
 }
 
 export async function write(db: PackageDB): Promise<void> {
-	return new Promise<void>((resolve, reject) => {
-		fs.writeFile('../npDatabase.json', JSON.stringify(db, null, 4), err => {
-			if (err) {
-				return reject(err);
-			}
-			resolve();
-		});
-	});
+    return new Promise<void>((resolve, reject) => {
+        fs.writeFile('../npDatabase.json', JSON.stringify(db, null, 4), err => {
+            if (err) {
+                return reject(err)
+            }
+            resolve()
+        })
+    })
 }
 
 export async function writeMods(db: PackageDB): Promise<void> {
-	const mods: ModDb = {};
+    const mods: ModDb = {}
 
-	for (const name of Object.keys(db)) {
-		const pkg = db[name];
+    for (const name of Object.keys(db)) {
+        const pkg = db[name]
+        const meta = pkg.metadata
+        const ccmod = pkg.metadataCCMod
 
-		if (pkg.metadata.ccmodType === 'base' || pkg.metadata.ccmodType === 'tool') {
-			continue;
-		}
+        if (meta?.ccmodType === 'base' || meta?.ccmodType === 'tool') continue
 
-		const install = getInstallation(pkg.installation);
-		if (!install) {
-			continue;
-		}
+        const install = getInstallation(pkg.installation)
+        if (!install) continue
 
-		mods[name] = {
-			name: pkg.metadata.ccmodHumanName || name,
-			description: pkg.metadata.description || 'A mod. (Description not available; contact mod author and have them add a description to their package.json file)',
-			license: pkg.metadata.license,
-			page: getHomepage(pkg.metadata.homepage),
-			archive_link: install.url,
-			hash: install.hash,
-			version: pkg.metadata.version,
-		};
-	}
+        mods[name] = {
+            name: getStringFromLocalisedString(ccmod?.title || meta!.ccmodHumanName || name),
+            description: getStringFromLocalisedString(
+                ccmod?.description ||
+                    meta!.description ||
+                    'A mod. (Description not available; contact mod author and have them add a description to their package.json file)'
+            ),
+            license: ccmod?.license || meta!.license,
+            page: getHomepage(ccmod?.homepage || meta!.homepage),
+            archive_link: install.url,
+            hash: install.hash,
+            version: ccmod?.version || meta!.version,
+        }
+    }
 
-	return new Promise<void>((resolve, reject) => {
-		fs.writeFile('../mods.json', JSON.stringify({mods}, null, 4), err => {
-			if (err) {
-				return reject(err);
-			}
-			resolve();
-		});
-	});
+    return new Promise<void>((resolve, reject) => {
+        fs.writeFile('../mods.json', JSON.stringify({ mods }, null, 4), err => {
+            if (err) {
+                return reject(err)
+            }
+            resolve()
+        })
+    })
 }
 
 function getHomepage(url?: string): Page[] {
-	if (!url) {
-		return [];
-	}
+    if (!url) {
+        return []
+    }
 
-	let name: string;
-	switch (new URL(url).hostname) {
-	case 'github.com':
-		name = 'GitHub';
-		break;
-	case 'gitlab.com':
-		name = 'GitLab';
-		break;
-	default:
-		name = 'mod\'s homepage';
-	}
+    let name: string
+    switch (new URL(url).hostname) {
+        case 'github.com':
+            name = 'GitHub'
+            break
+        case 'gitlab.com':
+            name = 'GitLab'
+            break
+        default:
+            name = "mod's homepage"
+    }
 
-	return [{name, url}];
+    return [{ name, url }]
 }
 
-function getInstallation(installations: InstallMethod[]): {url: string, hash: {sha256: string}} | undefined {
-	const zip = installations.find(i => i.type === 'ccmod') as InstallMethodCCMod;
-	if (zip) {
-		return {url: zip.url, hash: zip.hash};
-	}
-
-	const modZip = installations.find(i => i.type === 'modZip') as InstallMethodModZip;
-	if (modZip) {
-		return {url: modZip.url, hash: modZip.hash};
-	}
-
-	return undefined;
+function getStringFromLocalisedString(str: LocalizedString): string {
+    if (!str) throw new Error(`No mod name found: ${str}`)
+    if (typeof str === 'string') return str
+    const newStr = str.en_US
+    if (!newStr) throw new Error(`No english mod name found: ${str}`)
+    return newStr
 }
 
-async function buildEntry(result: PackageDB, pkg: PkgMetadata, inputs: InputLocation[]): Promise<void> {
-	result[pkg.name] = {
-		metadata: pkg,
-		installation: await generateInstallations(inputs),
-	};
+function getInstallation(installations: InstallMethod[]): { url: string; hash: { sha256: string } } | undefined {
+    const zip = installations.find(i => i.type === 'ccmod') as InstallMethodCCMod
+    if (zip) {
+        return { url: zip.url, hash: zip.hash }
+    }
+
+    const modZip = installations.find(i => i.type === 'modZip') as InstallMethodModZip
+    if (modZip) {
+        return { url: modZip.url, hash: modZip.hash }
+    }
+
+    return undefined
 }
 
-function check(pkg: PkgMetadata): boolean {
-	if (pkg.dependencies && !pkg.ccmodDependencies) {
-		console.warn(`Package has 'dependencies', not 'ccmodDependencies': ${pkg.name}; correct ASAP`);
-		return false;
-	}
+async function buildEntry(result: PackageDB, meta: PkgMetadata | undefined, ccmod: PkgCCMod | undefined, inputs: InputLocation[]): Promise<void> {
+    result[ccmod?.id || meta!.name] = {
+        metadata: meta,
+        metadataCCMod: ccmod,
+        installation: await generateInstallations(inputs),
+    }
+}
 
-	if (pkg.ccmodDependencies) {
-		if (pkg.ccmodDependencies.constructor !== Object) {
-			console.warn(`Package has dependencies not an object: ${pkg.name}`);
-			return false;
-		}
+function checkMeta(meta: PkgMetadata): boolean {
+    if (meta.dependencies && !meta.ccmodDependencies) {
+        console.warn(`Package has 'dependencies', not 'ccmodDependencies': ${meta.name}; correct ASAP`)
+        return false
+    }
 
-		for (let dep in pkg.ccmodDependencies) {
-			if (semver.validRange(pkg.ccmodDependencies[dep]) === null) {
-				console.warn(`Package has invalid constraint: ${pkg.name}`);
-				return false;
-			}
-		}
-	}
+    if (meta.ccmodDependencies) {
+        if (meta.ccmodDependencies.constructor !== Object) {
+            console.warn(`Package has dependencies not an object: ${meta.name}`)
+            return false
+        }
 
-	if (!pkg.version) {
-		console.warn(`Package is missing version: ${pkg.name}`);
-		return false;
-	}
+        for (let dep in meta.ccmodDependencies) {
+            if (semver.validRange(meta.ccmodDependencies[dep]) === null) {
+                console.warn(`Package has invalid constraint: ${meta.name}`)
+                return false
+            }
+        }
+    }
 
-	if (semver.parse(pkg.version) == null) {
-		console.warn(`Package version invalid: ${pkg.name}`);
-		return false;
-	}
+    if (!meta.version) {
+        console.warn(`Package is missing version: ${meta.name}`)
+        return false
+    }
 
-	return true;
+    if (semver.parse(meta.version) == null) {
+        console.warn(`Package version invalid: ${meta.name}`)
+        return false
+    }
+
+    return true
+}
+
+function checkCCMod(ccmod: PkgCCMod): boolean {
+    if (ccmod.dependencies) {
+        if (ccmod.dependencies.constructor !== Object) {
+            console.warn(`Package has dependencies not an object: ${ccmod.id}`)
+            return false
+        }
+
+        for (let dep in ccmod.dependencies) {
+            if (semver.validRange(ccmod.dependencies[dep]) === null) {
+                console.warn(`Package has invalid constraint: ${ccmod.id}`)
+                return false
+            }
+        }
+    }
+
+    if (!ccmod.version) {
+        console.warn(`Package is missing version: ${ccmod.id}`)
+        return false
+    }
+
+    if (semver.parse(ccmod.version) == null) {
+        console.warn(`Package version invalid: ${ccmod.id}`)
+        return false
+    }
+
+    return true
 }
 
 async function generateInstallations(inputs: InputLocation[]): Promise<InstallMethod[]> {
-	const result = [];
+    const result = []
 
-	for (const input of inputs) {
-		const install = await generateInstallation(input);
-		if (install) {
-			if (install instanceof Array) {
-				result.push(...install);
-			} else {
-				result.push(install);
-			}
-		}
-	}
+    for (const input of inputs) {
+        const install = await generateInstallation(input)
+        if (install) {
+            if (install instanceof Array) {
+                result.push(...install)
+            } else {
+                result.push(install)
+            }
+        }
+    }
 
-	return result;
+    return result
 }
 
 async function generateInstallation(input: InputLocation): Promise<InstallMethod[] | InstallMethod | undefined> {
-	switch (input.type) {
-	case 'modZip': {
-		const data = await streamToBuffer(await download(input.urlZip));
+    switch (input.type) {
+        case 'modZip': {
+            const data = await streamToBuffer(await download(input.urlZip))
 
-		return {
-			type: 'modZip',
-			url: input.urlZip,
-			source: input.source,
-			hash: {
-				sha256: crypto.createHash('sha256').update(data).digest('hex'),
-			},
-		};
-	}
-	case 'ccmod': {
-		const data = await streamToBuffer(await download(input.url));
+            return {
+                type: 'modZip',
+                url: input.urlZip,
+                source: input.source,
+                hash: {
+                    sha256: crypto.createHash('sha256').update(data).digest('hex'),
+                },
+            }
+        }
+        case 'ccmod': {
+            const data = await streamToBuffer(await download(input.url))
 
-		return {
-			type: 'ccmod',
-			url: input.url,
-			hash: {
-				sha256: crypto.createHash('sha256').update(data).digest('hex'),
-			},
-		};
-	}
-	case 'injected':
-		return input.installation;
-	}
+            return {
+                type: 'ccmod',
+                url: input.url,
+                hash: {
+                    sha256: crypto.createHash('sha256').update(data).digest('hex'),
+                },
+            }
+        }
+        case 'injected':
+            return input.installation
+    }
 }
 
-function groupByName(packages: [PkgMetadata, InputLocation][]) : Map<string, [PkgMetadata, InputLocation[]]> {
-	const result = new Map<string, [PkgMetadata, InputLocation[]]>();
+function groupByName(packages: ModMetadatasInput[]): Map<string, ModMetadatas & { inputs: InputLocation[] }> {
+    const result = new Map<string, ModMetadatas & { inputs: InputLocation[] }>()
 
-	for (const [pkg, input] of packages) {
-		if (result.has(pkg.name)) {
-            result.get(pkg.name)?.[1].push(input);
-		} else {
-			result.set(pkg.name, [pkg, [input]]);
-		}
-	}
+    for (const { meta: pkg, ccmod, input } of packages) {
+        for (const name of [ccmod?.id, pkg?.name, 'nomodlikethiseverwillexist'].filter(Boolean) as string[]) {
+            if (name == 'nomodlikethiseverwillexist') throw new Error(packages.join())
+            if (result.has(name)) {
+                result.get(name)?.inputs.push(input)
+            } else {
+                result.set(name, { meta: pkg, ccmod, inputs: [input] })
+            }
+            break
+        }
+    }
 
-	return result;
+    return result
 }
 
 function sort(db: PackageDB): PackageDB {
-	const result: PackageDB = {};
-	for (const key of Object.keys(db).sort()) {
-		result[key] = db[key];
-	}
-	return result;
+    const result: PackageDB = {}
+    for (const key of Object.keys(db).sort()) {
+        result[key] = db[key]
+    }
+    return result
 }
--- build/src/types.d.ts
+++ build/src/types.d.ts
@@ -1,39 +1,38 @@
-declare type InputLocation = InjectedInputLocation | ModZipInputLocation | CCModInputLocation;
+declare type InputLocation = InjectedInputLocation | ModZipInputLocation | CCModInputLocation
 
 // Represents a raw input location to put more or less directly into npDatabase.json
 declare type InjectedInputLocation = {
-	type: 'injected';
-} & Package;
+    type: 'injected'
+} & Package
 
 declare type ModZipInputLocation = {
-	type: 'modZip';
-	// The URL of the ZIP file.
-	urlZip: string;
-	// The subdirectory in which the package.json file is kept.
-	// Note that GitHub archives have an additional enclosing directory, so you will usually need to use this.
-	// Only this subdirectory & subdirectories of it are extracted, and it is extracted at the target installation directory.
-	source?: string;
-	// If provided, then the package.json file is at this location in the archive, regardless of 'source'.
-	// This must pretty much only be used for base packages.
-	packageJSONPath?: string;
-};
+    type: 'modZip'
+    // The URL of the ZIP file.
+    urlZip: string
+    // The subdirectory in which the package.json file is kept.
+    // Note that GitHub archives have an additional enclosing directory, so you will usually need to use this.
+    // Only this subdirectory & subdirectories of it are extracted, and it is extracted at the target installation directory.
+    source?: string
+    // If provided, then the package.json file is at this location in the archive, regardless of 'source'.
+    // This must pretty much only be used for base packages.
+    packageJSONPath?: string
+}
 
 declare type CCModInputLocation = {
-	type: 'ccmod';
-	// The URL of the ccmod file.
-	url: string;
+    type: 'ccmod'
+    // The URL of the ccmod file.
+    url: string
 }
 
 // The content of the input-locations.json file.
-declare type InputLocations = InputLocation[];
-
+declare type InputLocations = InputLocation[]
 
 // An os.platform() value (see https://nodejs.org/api/os.html#os_os_platform )
-declare type NodeOSPlatform = string;
+declare type NodeOSPlatform = string
 
 // Imported from https://github.com/CCDirectLink/CLS/blob/master/proposals/1/standardized-mod-format.md
-declare type Semver = string;
-declare type SemverConstraint = string;
+declare type Semver = string
+declare type SemverConstraint = string
 // StandardizedModPackage is retroactively made a subclass of PackageDBPackageMetadata.
 
 /*
@@ -42,16 +41,16 @@ declare type SemverConstraint = string;
  *  and special-case UI to be user-friendly, such as CCLoader and NWJS upgrades.
  * (In particular, CrossCode, CCLoader and NWJS upgrades require custom detection methods for their local copies.)
  */
-declare type PackageType = 'mod' | 'tool' | 'base';
+declare type PackageType = 'mod' | 'tool' | 'base'
 
 /*
  * A page relating to the mod.
  */
 declare type Page = {
-	// The name of the page. For the canonical GitHub or GitLab page, this must be "GitHub" / "GitLab".
-	name: string;
-	url: string;
-};
+    // The name of the page. For the canonical GitHub or GitLab page, this must be "GitHub" / "GitLab".
+    name: string
+    url: string
+}
 
 /*
  * This is related to the supported package metadata for mods, on purpose.
@@ -62,80 +61,120 @@ declare type Page = {
  *  and that means ensuring all package metadata is either avoided by NPM or understood by it.
  */
 declare type PkgMetadata = {
-	// This is the unique ID for this package, used for dependency handling. Note that this DOES NOT have to avoid collision with the NPM registry.
-	name: string;
-	// If not provided, defaults to "mod".
-	ccmodType?: PackageType;
-	// This is the version of the package for dependency purposes. (see https://docs.npmjs.com/files/package.json )
-	version: Semver;
-	// This is the dependencies of the package, if any. If not present, `dependencies` is checked.
-	// If `dependencies` contains NPM packages, you must supply at least an empty object here to prevent issues.
-	ccmodDependencies?: Record<string, SemverConstraint>;
-	// This is the dependencies of the package, if any. WARNING: THIS CONFLICTS WITH NPM. DO NOT USE. NEW MODS WITH THIS WILL NOT BE ACCEPTED.
-	dependencies?: Record<string, SemverConstraint>;
+    // This is the unique ID for this package, used for dependency handling. Note that this DOES NOT have to avoid collision with the NPM registry.
+    name: string
+    // If not provided, defaults to "mod".
+    ccmodType?: PackageType
+    // This is the version of the package for dependency purposes. (see https://docs.npmjs.com/files/package.json )
+    version: Semver
+    // This is the dependencies of the package, if any. If not present, `dependencies` is checked.
+    // If `dependencies` contains NPM packages, you must supply at least an empty object here to prevent issues.
+    ccmodDependencies?: Record<string, SemverConstraint>
+    // This is the dependencies of the package, if any. WARNING: THIS CONFLICTS WITH NPM. DO NOT USE. NEW MODS WITH THIS WILL NOT BE ACCEPTED.
+    dependencies?: Record<string, SemverConstraint>
 
-	// Below here is metadata that has no effect on function.
+    // Below here is metadata that has no effect on function.
 
-	// This is the name the user is supposed to see. Really meant as a preservation mechanism for old mods.json naming information. Defaults to name.
-	ccmodHumanName?: string;
-	// This is the description of the package. (see https://docs.npmjs.com/files/package.json )
-	description?: string;
-	// SPDX license identifier or human readable text (see https://docs.npmjs.com/files/package.json )
-	license?: string;
-	// Homepage URL (see https://docs.npmjs.com/files/package.json )
-	homepage?: string;
-};
+    // This is the name the user is supposed to see. Really meant as a preservation mechanism for old mods.json naming information. Defaults to name.
+    ccmodHumanName?: string
+    // This is the description of the package. (see https://docs.npmjs.com/files/package.json )
+    description?: string
+    // SPDX license identifier or human readable text (see https://docs.npmjs.com/files/package.json )
+    license?: string
+    // Homepage URL (see https://docs.npmjs.com/files/package.json )
+    homepage?: string
+}
+
+type FilePath = string
+
+type LocalizedString = Record<Locale, string> | string
+type Locale = string
+
+type Person = PersonDetails | string
+interface PersonDetails {
+    name: LocalizedString
+    email?: LocalizedString
+    url?: LocalizedString
+    comment?: LocalizedString
+}
+
+declare type PkgCCMod = {
+    id: string
+    version?: Semver
+
+    title?: LocalizedString
+    description?: LocalizedString
+    license?: string
+    homepage?: string
+    keywords?: string[]
+    authors?: Person[]
+    icons?: Record<string, FilePath>
+    type?: 'mod' | 'library'
+
+    dependencies?: Record<string, SemverConstraint>
+
+    assets?: FilePath[]
+    assetsDir?: FilePath
+    modPrefix?: string
+
+    plugin?: FilePath
+    preload?: FilePath
+    postload?: FilePath
+    prestart?: FilePath
+    poststart?: FilePath
+}
 
 // Represents some set of hashes for something.
 declare type PkgHash = {
-	// Lowercase hexadecimal-encoded SHA-256 hash of the data.
-	sha256: string;
-};
+    // Lowercase hexadecimal-encoded SHA-256 hash of the data.
+    sha256: string
+}
 
 /*
  * Represents a method of installing the package.
  */
-declare type InstallMethod = InstallMethodCommon | InstallMethodModZip | InstallMethodCCMod;
+declare type InstallMethod = InstallMethodCommon | InstallMethodModZip | InstallMethodCCMod
 
 /*
  * The common fields between all PackageDBInstallationMethods.
  */
 declare type InstallMethodCommon = {
-	// Declares the type of installation method. ALWAYS CHECK THIS.
-	type: string;
-	// If present, constrains the platform this method may be used for.
-	platform?: NodeOSPlatform;
+    // Declares the type of installation method. ALWAYS CHECK THIS.
+    type: string
+    // If present, constrains the platform this method may be used for.
+    platform?: NodeOSPlatform
 }
 
 declare type InstallMethodModZip = InstallMethodCommon & {
-	type: 'modZip';
-	// The URL of the ZIP to download. (example: "https://github.com/CCDirectLink/CCLoader/archive/master.zip")
-	url: string;
-	// The hash of the file at url.
-	hash: PkgHash;
-	// If provided, the subdirectory of the ZIP that is the root of the extraction (example: "CCLoader-master")
-	source?: string;
-};
+    type: 'modZip'
+    // The URL of the ZIP to download. (example: "https://github.com/CCDirectLink/CCLoader/archive/master.zip")
+    url: string
+    // The hash of the file at url.
+    hash: PkgHash
+    // If provided, the subdirectory of the ZIP that is the root of the extraction (example: "CCLoader-master")
+    source?: string
+}
 
 declare type InstallMethodCCMod = InstallMethodCommon & {
-	type: 'ccmod';
-	// The URL of the ccmod to download. (example: "https://github.com/CCDirectLink/CC-ChargedBalls/releases/download/1.0.0/ChargedBalls.ccmod")
-	url: string;
-	// The hash of the file at url.
-	hash: PkgHash;
+    type: 'ccmod'
+    // The URL of the ccmod to download. (example: "https://github.com/CCDirectLink/CC-ChargedBalls/releases/download/1.0.0/ChargedBalls.ccmod")
+    url: string
+    // The hash of the file at url.
+    hash: PkgHash
 }
 
 /*
  * Represents a package in the database.
  */
 declare type Package = {
-	// Metadata for the package.
-	metadata: PkgMetadata;
-	// Installation methods (try in order)
-	installation: InstallMethod[];
-};
+    // Metadata for the package.
+    metadata?: PkgMetadata
+    metadataCCMod?: PkgCCMod
+    // Installation methods (try in order)
+    installation: InstallMethod[]
+}
 
 /*
  * Represents the database. Keys in this Record MUST match their respective `value.metadata.name`
  */
-declare type PackageDB = Record<string, Package>;
+declare type PackageDB = Record<string, Package>
